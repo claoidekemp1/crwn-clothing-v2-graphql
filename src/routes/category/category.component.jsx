3420964af6d7bc2d598b3809629d42e952545555
claoidekemp1 <85615173+claoidekemp1@users.noreply.github.com>
2023-08-15 16:57:25 +0100

    Section 26

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,6 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
@@ -7,24 +8,59 @@ import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
 
+import Spinner from '../../components/spinner/spinner.component';
+
+const GET_CATEGORY = gql `
+  query($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
 
+  const {loading, error, data} = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    }
+  })
+
+  console.log(data)
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if(data) {
+      const {
+        getCollectionsByTitle: { items}
+      } = data;
+      setProducts(items)
+    }
+  })
+
+  const [products, setProducts] = useState([]);
 
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {loading ? (
+        <Spinner />
+      ): (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )
+      }
+
     </Fragment>
   );
 };
